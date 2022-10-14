c0217883c30d1ed9991316feeed19732ccfe852f
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-14 19:43:10 -0300

    Requisito 20

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputSenha = document.getElementById('input-senha');
 const botaoEntrar = document.querySelector('#botaoEntrar');
 const checkAgreement = document.querySelector('#agreement');
 const botaoSubmit = document.querySelector('#submit-btn');
+const elementTextarea = document.getElementById('textarea');
 
 botaoEntrar.addEventListener('click', () => {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -14,4 +15,13 @@ botaoEntrar.addEventListener('click', () => {
 
 checkAgreement.addEventListener('change', () => {
   botaoSubmit.disabled = !checkAgreement.checked;
+});
+
+elementTextarea.addEventListener('keyup', () => {
+  let contador = document.getElementById('counter');
+  contador.innerHTML = '500';
+  let numeroMaxCaracteres = elementTextarea.maxLength;
+  let numeroCaracteres = elementTextarea.value.length;
+  let totalCaracteres = numeroMaxCaracteres - numeroCaracteres;
+  contador.innerHTML = JSON.stringify(totalCaracteres);
 });
