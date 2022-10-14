549e9beb9172c020af75eb6023a19bbe8990bbe9
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-14 15:54:11 -0300

    Requisito 3: concluído

---
+++ script.js
@@ -0,0 +1,15 @@
+const inputEmail = document.getElementById('input-email');
+const inputSenha = document.getElementById('input-senha');
+const botaoEntrar = document.querySelector('#botaoEntrar');
+
+botaoEntrar.addEventListener('click', () => {
+  if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+});
+
+
+
+
