a41898a403189b8373091df82442751f9d5eab7e
Sthéfani Martinelli <sthefani_martinelli@hotmail.com>
2022-10-18 22:09:07 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -4,6 +4,20 @@ const botaoEntrar = document.querySelector('#botaoEntrar');
 const checkAgreement = document.querySelector('#agreement');
 const botaoSubmit = document.querySelector('#submit-btn');
 const elementTextarea = document.getElementById('textarea');
+const emailDigitado = document.getElementById('input-email');
+const nomeDigitado = document.getElementById('input-name');
+const sobrenomeDigitado = document.getElementById('input-lastname');
+const casaSelecionada = document.querySelector('#house');
+const comentario = document.querySelector('textarea');
+const primeiroForm = document.getElementById('evaluation-form');
+const formularioDados = document.getElementById('form-data');
+const nomeInserido = document.getElementById('nome-inserido');
+const emailInserido = document.getElementById('email-inserido');
+const casaInserida = document.getElementById('casa-inserida');
+const familiaInserida = document.getElementById('familia-inserida');
+const materiasInseridas = document.getElementById('materias-inseridas');
+const avaliacaoInserida = document.getElementById('avaliacao-inserida');
+const comentarioInserido = document.getElementById('comentario-inserido');
 
 botaoEntrar.addEventListener('click', () => {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
@@ -24,4 +38,31 @@ elementTextarea.addEventListener('keyup', () => {
   const numeroCaracteres = elementTextarea.value.length;
   const totalCaracteres = numeroMaxCaracteres - numeroCaracteres;
   contador.innerHTML = JSON.stringify(totalCaracteres);
+});
+
+const selectedSubjects = () => {
+  const materiasSelecionadas = document.querySelectorAll('input[class="subject"]:checked');
+  let arrayMaterias = '';
+  for (let index = 0; index < materiasSelecionadas.length; index += 1) {
+    arrayMaterias = `${arrayMaterias} ${(materiasSelecionadas[index].value)},`;
+  }
+  return arrayMaterias;
+};
+
+botaoSubmit.addEventListener('click', (event) => {
+  event.preventDefault();
+  const familiaSelecionada = document.querySelector('input[name="family"]:checked');
+  const notaSelecionada = document.querySelector('input[name="rate"]:checked');
+  // selectedSubjects();
+
+  primeiroForm.style.display = 'none';
+  formularioDados.style.display = 'flex';
+  formularioDados.style.backgroundColor = 'orange';
+  nomeInserido.innerHTML = `Nome: ${nomeDigitado.value} ${sobrenomeDigitado.value}`;
+  emailInserido.innerHTML = `Email: ${emailDigitado.value}`;
+  casaInserida.innerHTML = `Casa: ${casaSelecionada.value}`;
+  familiaInserida.innerHTML = `Família: ${familiaSelecionada.value}`;
+  materiasInseridas.innerHTML = `Matérias: ${selectedSubjects()}`;
+  avaliacaoInserida.innerHTML = `Avaliação: ${notaSelecionada.value}`;
+  comentarioInserido.innerHTML = `Observações: ${comentario.value}`;
 });
